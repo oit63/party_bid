b887bd67066a7a28a3743c8b09db68be9309037a
TanghaoTsui <TanghaoTsui@jotekiMacBook-Pro.local>
2014-07-23 16:17:11 +0800

    增添了 活动穿件页面上开始结束的判断

--- app/scripts/controllers/activity_sign_up_controller.js
+++ app/scripts/controllers/activity_sign_up_controller.js
@@ -10,24 +10,31 @@ angular.module('partyBidApp')
             'AngularJS',
             'Karma'
         ];
-        $scope.startButton = "开始";
         $scope.go_activity_list = function(){
             $location.path('/activity_list');};
-        $scope.start = function(){
+
+        $scope.state = function(){
+            var state = localStorage.getItem('state')|| "开始";
+            if(state == "开始")
+            {
+                state = "结束";
+                $scope.stateButton = "结束" ;
+            }
+            else{
+                state = "开始"
+                $scope.stateButton = "开始" ;
+            }
+            localStorage.setItem('state', state);
+//            if(state){$scope.stateButton = "结束";}
+//            else{$scope.stateButton = "开始";}
+//            if($scope.stateButton = "结束")
+//            {$scope.stateButton = "开始";}
+//            if($scope.stateButton = "开始")
+//            {$scope.stateButton = "结束";}
             var currentActive = localStorage.getItem('yourChoice');
             localStorage.setItem('currentActive', currentActive);
             $location.path('activity_sign_up');
-            if($scope.startButton = "开始")
-            {
-                $scope.startButton = "结束";
-            }
-            else
-            {
-                $scope.startButton = "开始";
-            }
-
-
         };
-
+        $scope.stateButton = localStorage['state'] || "开始";
 
     })
--- app/scripts/controllers/sms.js
+++ app/scripts/controllers/sms.js
@@ -2,8 +2,8 @@
 //notify_message_received({"messages":[{"create_date":"Tue Jan 15 15:28:44 格林尼治标准时间+0800 2013","message":"jj308","phone":"18733171780"}]})
 var native_accessor = {
     send_sms: function (phone, message) {
-        native_access.send_sms({"receivers":[{"name":'name', "phone":phone}]}, {"message_content":message});
-        console.log(phone, message);
+//        native_access.send_sms({"receivers":[{"name":'name', "phone":phone}]}, {"message_content":message});
+//        console.log(phone, message);
 
     },
 
@@ -34,9 +34,8 @@ var native_accessor = {
 
     process_received_message: function (json_message)
     {
-        native_accessor.send_sms(phone, message);
-
-        console.log("process_received 收到!");
+//        native_accessor.send_sms(phone, message);
+        console.log("报名成功!");
     }
 };
 
