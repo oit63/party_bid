9641b6fe4ff1465b1b2875d2fc7a7acee0d3d815
tanghaotsui <TanghaoTsui@jotekiMacBook-Pro.local>
2014-07-30 15:58:46 +0800

    ”初始化按钮”

--- app/scripts/controllers/activity_sign_up_controller.js
+++ app/scripts/controllers/activity_sign_up_controller.js
@@ -19,7 +19,25 @@ angular.module('partyBidApp')
         {
             localStorage['state'] = "stop";
         }
-
+        //初始化按钮
+        if(localStorage['state'] == "running")//有活动在运行
+        {
+            if(localStorage['currentActive'] == localStorage['yourChoice'])//就是这个活动在运行
+            {
+                $scope.in_state = "recieving";
+            }
+            if(localStorage['currentActive'] !== localStorage['yourChoice'])//不是这个活动在运行
+            {
+                $scope.in_state = "grey";
+            }
+        }
+        else//没有活动在运行
+        {
+            console.log("1.2")
+            $scope.in_state = "end";
+        }
+        //读取数据
+        $scope.informations = sms_data_filted();
         //-----------------------------------------//
         $scope.go_activity_list = function()
         {
@@ -29,23 +47,20 @@ angular.module('partyBidApp')
         $scope.disableFlag = judge_grey();
         $scope.get_in_start_state = function()
         {
-            $scope.in_state = "end";
+            $scope.in_state = "recieving";
             localStorage["state"] = "running";
             setCurrentAsChoice();
             //-----------------------------//
             var sms_data_process = JSON.parse(localStorage['sms_data']);
-            $scope.informations = sms_data_filted();
         };
         $scope.get_in_end_state = function()
         {
             if(confirm("报名结束确认?"))
             {
-                $scope.in_state = "recieving";
+                $scope.in_state = "end";
                 localStorage["state"] = "stop";
             }
         };
-        //-----------------------------------------//
-
         //-----------------------------------------//
     })
 
